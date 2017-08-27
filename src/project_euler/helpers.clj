7c4c319dd7338ca5e1fb0e81394133594d30a275
Harmen Janssen <harmen@grrr.nl>
2017-08-27 21:14:52 +0200

    Elaborated factors function to be more robust

--- src/project_euler/helpers.clj
+++ src/project_euler/helpers.clj
@@ -1,16 +1,38 @@
 (ns project-euler.helpers
   (:use [clojure.math.numeric-tower :as math]))
 
+(defn lte
+  [x y]
+  (or (= x y) (< x y)))
+
+(defn gte
+  [x y]
+  (or (= x y) (> x y)))
+
+(gte 11 12)
+
 (defn divisible?
   [a b]
   (zero? (mod a b)))
 
+(defn- add-dual-factors
+  "Conj two parts of a number's factor to the list.
+  For instance: 2 is a factor of 24. When given 2 and 24, this function
+  will add 2 and 12 to the list."
+  [lst number factor]
+  (conj (conj lst factor) (/ number factor)))
+
 (defn factors
-  [n]
-  (filter (partial divisible? n) (range 1 (math/sqrt n))))
+  "Lists a number's factors"
+  [target]
+  (loop [lst []
+         n 1]
+    (if (gte n (math/ceil (math/sqrt target)))
+      lst
+      (recur
+        (if (divisible? target n) (add-dual-factors lst target n) lst)
+        (inc n)))))
 
 (defn prime?
   [n]
-  (= 1 (count (factors n))))
-
-
+  (= 2 (count (factors n))))
