f735121c1b251053e04e01458e18b8f3645be609
Harmen Janssen <harmen@grrr.nl>
2017-09-01 22:46:12 +0200

    Solved problem 004

--- src/project_euler/problem004.clj
+++ src/project_euler/problem004.clj
@@ -1,26 +1,14 @@
-(ns project-euler.problem003
+(ns project-euler.problem004
   (:use [project-euler.helpers :refer :all]))
 
 (defn palindrome?
   [n]
   (= (str n) (clojure.string/join (reverse (str n)))))
 
-(defn 3-digit-products
-  ([a b] (3-digit-products a b []))
-  ([a b lst]
-    (if (and (= a 999) (> 20 10))
-      lst
-      (cons (* a b) (3-digit-products (inc a) b lst))))
+(def three-digit-products (for [x (range 100 999) y (range 100 999) :when (< x y)] (* x y)))
 
-; NOTE: waarschijnlijk beter om achterstevoren te kijken, want het gaat om de hoogste,
-; dus het eerste palindroom dat we vinden is de juiste?
-; Tevens: je hoeft niet beide getallen helemaal af te lopen, want 100 * 999 is hetzelfde
-; als 999 * 100
-
-(take 100 (3-digit-products 100 100))
-
-(defn problem003
+(defn problem004
   "A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
   Find the largest palindrome made from the product of two 3-digit numbers."
   []
-  ())
+  (reduce max (filter palindrome? three-digit-products)))
--- test/project_euler/core_test.clj
+++ test/project_euler/core_test.clj
@@ -2,10 +2,12 @@
   (:require [clojure.test :refer :all]
             [project-euler.problem001 :refer [problem001]]
             [project-euler.problem002 :refer [problem002]]
-            [project-euler.problem003 :refer [problem003]]))
+            [project-euler.problem003 :refer [problem003]]
+            [project-euler.problem004 :refer [problem004]]))
 
 (deftest all-solutions
   (testing "All solutions"
     (is (= 233168 (problem001)))
     (is (= 4613732 (problem002)))
-    (is (= 6857 (problem003)))))
+    (is (= 6857 (problem003)))
+    (is (= 906609 (problem004)))))
