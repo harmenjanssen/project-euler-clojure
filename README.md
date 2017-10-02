f06cebd29c6f8caaf5a3cfa83a5f0aa4c4a1576b
Harmen Janssen <harmen@grrr.nl>
2017-10-02 22:04:19 +0200

    Added solution runner for lein

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Project Euler
 
 Doing [Project Euler](https://projecteuler.net/) exercises in Clojure.
+
+Run a single solution using `lein run [number]`. 
+
+Or run the tests (prepare to wait a long while!): `lein test`.
--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -1,6 +1,20 @@
-(ns project-euler.core
-  (:gen-class))
+(ns project-euler.core)
+
+; Note: I found the following over here: https://github.com/guillaume-nargeot/project-euler-clojure
+; I liked it so much I stole it. Thanks @guillaume-nargeot!
+
+(set! *warn-on-reflection* true)
+
+(defn run-problem [n]
+  (let [number (.format (new java.text.DecimalFormat "000") n)
+        solution-function (symbol (str "problem" number))]
+    (use (vec (list (symbol (str "project-euler.problem" number))
+                    :only (list solution-function))))
+    (eval (list solution-function))))
 
 (defn -main
+  "Solution runner"
   [& args]
-  (println "Run the tests why don't you?"))
+  (if (and args (= 1 (count args)))
+    (println (time (run-problem (Integer/parseInt (first args)))))
+    (println "Usage: lein run [number]")))
