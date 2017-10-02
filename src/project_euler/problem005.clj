aead51f73c1f3c56158e6912ff64f2d332ac2275
Harmen Janssen <harmen@grrr.nl>
2017-10-02 09:47:09 +0200

    Finished problem 005

---
+++ src/project_euler/problem005.clj
@@ -0,0 +1,17 @@
+(ns project-euler.problem005
+  (:use [project-euler.helpers :refer :all]))
+
+(defn divisible-by-all?
+  "true if n is cleanly divisible by all numbers up to and including total"
+  [total n]
+  (every? (partial divisible? n) (range 1 (inc total))))
+
+(defn find-first
+  [f coll]
+  (first (filter f coll)))
+
+(defn problem005
+  "2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"
+  [n]
+  (find-first (partial divisible-by-all? n) (iterate (partial + n) n)))
--- test/project_euler/core_test.clj
+++ test/project_euler/core_test.clj
@@ -3,11 +3,15 @@
             [project-euler.problem001 :refer [problem001]]
             [project-euler.problem002 :refer [problem002]]
             [project-euler.problem003 :refer [problem003]]
-            [project-euler.problem004 :refer [problem004]]))
+            [project-euler.problem004 :refer [problem004]]
+            [project-euler.problem005 :refer [problem005]]))
 
 (deftest all-solutions
   (testing "All solutions"
     (is (= 233168 (problem001)))
     (is (= 4613732 (problem002)))
     (is (= 6857 (problem003)))
-    (is (= 906609 (problem004)))))
+    (is (= 906609 (problem004)))
+    (is (= 232792560 (problem005 20)))
+  )
+)
